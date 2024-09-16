7aa4d1a3403e32b28fd0b777e862d8666bf67e06
tomacho <tomacho28.th@gmail.com>
2024-09-16 10:18:59 -0300

    agregamos validaciones formulario

--- src/Componentes/Formulario.jsx
+++ src/Componentes/Formulario.jsx
@@ -1,19 +1,50 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import Lista from "./Lista"
+import Lista from "./Lista";
 import Itemtarea from "./Itemtarea";
+import { useForm } from "react-hook-form";
 
 const Formulario = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <section>
-      <Form>
+      <Form onSubmit={handleSubmit(onSubmit)} className="mb-3">
         <Form.Group className="mb-3 d-flex" controlId="formBasicEmail">
-        <Form.Control type="text" placeholder="tarea" />
-        <Button variant="primary" type="submit" className="ms-2">
-          Enviar
-        </Button>
+          <Form.Control
+            type="text"
+            placeholder="tarea"
+            {...register("tarea", {
+              required: "La tarea es un dato obligatorio",
+              minLength: {
+                value: 2,
+                message: "La tarea debe tener como minimo 2 caracteres",
+              },
+              maxLength: {
+                value: 20,
+                message: "La tarea debe tener como maximo 20 caracteres",
+              },
+            })}
+          />
+          <Button variant="primary" type="submit" className="ms-2">
+            Enviar
+          </Button>
         </Form.Group>
+        <Form.Text className="text-danger mb-3">
+          {errors.tarea?.message}
+        </Form.Text>
       </Form>
+      <Lista></Lista>
+      <Lista></Lista>
+      <Lista></Lista>
       <Lista></Lista>
     </section>
   );
